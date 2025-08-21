270b89f413329bcc15a65d66aadd14d36c28b63b
Benja85a <rivasbenja97@gmail.com>
2025-08-21 14:07:15 -0600

    creating: dist

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { assets, recursos } from "../assets/assets";
+import { assets, recursos } from "../assets/assets.js";
 import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
